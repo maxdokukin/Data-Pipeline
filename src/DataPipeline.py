ed3bbe9d3cc81ccbe99ad0ec1441f997ec2de663
Max <tools.shiny.01@icloud.com>
2024-07-17 15:02:21 -0700

    working fine

--- src/DataPipeline.py
+++ src/DataPipeline.py
@@ -1,19 +1,19 @@
 import os
 
 class DataPipeline:
-    def __init__(self, base_directory, verbose=False, stack_stages_name=False):
+    def __init__(self, base_directory, start_folder, verbose=False, stacked_stages_names_outpud=False):
         self.base_directory = base_directory
         self.stages = []
-        self.current_directory = f"{base_directory}_original"
+        self.current_directory = os.path.join(base_directory, start_folder)
         self.verbose = verbose
         self.stage_control = {}
-        self.stack_stages_name = stack_stages_name
-        self.current_stage_names = []
+        self.stack_stages_names = stacked_stages_names_outpud
+        self.executed_stages_stack = []
 
-    def add_stage(self, stage_name, function_pointer, description=""):
+    def add_stage(self, stage_name, function_pointer, output_name=""):
         stage_id = len(self.stages)
         self.stages.append(
-            {'id': stage_id, 'name': stage_name, 'function': function_pointer, 'description': description})
+            {'id': stage_id, 'name': stage_name, 'function': function_pointer, 'output_name': output_name})
         self.stage_control[stage_id] = True  # Enable the stage by default
 
     def enable_stage(self, stage_id, execute):
@@ -32,19 +32,19 @@ class DataPipeline:
 
             stage_name = stage_info['name']
             function = stage_info['function']
-            description = stage_info['description']
+            output_name = stage_info['output_name']
 
-            if self.stack_stages_name:
-                self.current_stage_names.append(stage_name)
-                next_directory = os.path.join(self.base_directory, '_'.join(self.current_stage_names))
+            if self.stack_stages_names:
+                self.executed_stages_stack.append(output_name)
+                next_directory = os.path.join(self.base_directory, '_'.join(self.executed_stages_stack))
             else:
-                next_directory = os.path.join(self.base_directory, f"stage_{stage_id}")
+                next_directory = os.path.join(self.base_directory, output_name)
 
             if not os.path.exists(next_directory):
                 os.makedirs(next_directory)
 
             if self.verbose:
-                print(f"Stage {stage_id}: {stage_name}")
+                print(f"Stage {stage_id}: {stage_name}, {output_name}")
                 print(f" - Input directory: {self.current_directory}")
                 print(f" - Output directory: {next_directory}")
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,57 @@
 from DataPipeline import DataPipeline
+
+# Define your external functions
+def function0(input_directory, output_directory):
+    # Implementation for stage 0
+    # if not os.path.exists(output_directory):
+    #     os.makedirs(output_directory)
+    # for filename in os.listdir(input_directory):
+    #     src = os.path.join(input_directory, filename)
+    #     dst = os.path.join(output_directory, filename)
+    #     # Implement the renaming logic
+    #     # For example:
+    #     os.rename(src, dst)
+    print("function 0 called")
+
+
+def function1(input_directory, output_directory):
+    # Implementation for stage 1
+    print("function 1 called")
+
+
+def function2(input_directory, output_directory):
+    # Implementation for stage 2
+    print("function 2 called")
+
+def function3(input_directory, output_directory):
+    # Implementation for stage 2
+    print("function 3 called")
+
+def function4(input_directory, output_directory):
+    # Implementation for stage 2
+    print("function 4 called")
+
+# Usage
+pipeline = DataPipeline(
+    base_directory=r'C:\Users\mdokukin1\Desktop\GitHub\Data-Pipeline\data',
+    start_folder='original',
+    verbose=True,
+    stacked_stages_names_outpud=False
+)
+
+pipeline.add_stage('rename', function0, 'renamed')
+pipeline.add_stage('trim', function1, 'trimmed')
+pipeline.add_stage('normalize', function2, 'normalized')
+pipeline.add_stage('split', function3, 'split')
+pipeline.add_stage('augment', function4, 'augmented')
+
+# Enable or disable specific stages
+pipeline.enable_stage(0, True)  # Execute stage 0
+pipeline.enable_stage(1, True)  # Skip stage 1
+pipeline.enable_stage(2, True)  # Execute stage 2
+pipeline.enable_stage(3, True)  # Execute stage 2
+pipeline.enable_stage(4, True)  # Execute stage 2
+
+pipeline.execute_pipeline()
+# data = ...  # Load your data here
+# processed_data = pipeline.execute_pipeline(data)
