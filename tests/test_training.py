0348c1e1bba5b5c6ae1f323b2a5404f96b64c008
Max <tools.shiny.01@icloud.com>
2024-07-17 15:39:17 -0700

    added pre and post stage functions tested; works

--- tests/test_training.py
+++ tests/test_training.py
@@ -3,37 +3,35 @@ from DataPipeline import DataPipeline
 # Define your external functions
 def function0(input_directory, output_directory):
     # Implementation for stage 0
-    # if not os.path.exists(output_directory):
-    #     os.makedirs(output_directory)
-    # for filename in os.listdir(input_directory):
-    #     src = os.path.join(input_directory, filename)
-    #     dst = os.path.join(output_directory, filename)
-    #     # Implement the renaming logic
-    #     # For example:
-    #     os.rename(src, dst)
-    print("function 0 called")
-
+    print("Function 0 called")
 
 def function1(input_directory, output_directory):
     # Implementation for stage 1
-    print("function 1 called")
-
+    print("Function 1 called")
 
 def function2(input_directory, output_directory):
     # Implementation for stage 2
-    print("function 2 called")
+    print("Function 2 called")
 
 def function3(input_directory, output_directory):
-    # Implementation for stage 2
-    print("function 3 called")
+    # Implementation for stage 3
+    print("Function 3 called")
 
 def function4(input_directory, output_directory):
-    # Implementation for stage 2
-    print("function 4 called")
+    # Implementation for stage 4
+    print("Function 4 called")
 
 def function5(input_directory, output_directory):
-    # Implementation for stage 2
-    print("function 5 called")
+    # Implementation for stage 5
+    print("Function 5 called")
+
+# Define pre and post stage functions
+def pre_stage_function(stage_name, current_directory):
+    print(f"Pre-stage function called")
+
+def post_stage_function(stage_name, current_directory):
+    print(f"Post-stage function called")
+
 # Usage
 pipeline = DataPipeline(
     base_directory=r'C:\Users\mdokukin1\Desktop\GitHub\Data-Pipeline\data',
@@ -49,9 +47,12 @@ pipeline.add_stage('convert_tf_tflite', function3, 'tflite')
 pipeline.add_stage('test_tflite', function4, '')
 pipeline.add_stage('convert_tflite_c', function5, 'converted')
 
+# Set pre and post stage functions
+pipeline.set_prestage_function(pre_stage_function)
+pipeline.set_poststage_function(post_stage_function)
+
 pipeline.disable_stage('train')
 pipeline.disable_stage('test_tf')
 pipeline.disable_stage('optimize')
 
 pipeline.execute()
-
